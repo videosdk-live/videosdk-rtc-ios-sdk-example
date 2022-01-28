b6bc7928c64b08e983dbc22bcf482c629d0f69c1
Rushi Sangani <rushi@rosterbuster.com>
2022-01-28 14:47:17 +0530

    storyboard update

--- VideoSDK/API/Constants.swift
+++ VideoSDK/API/Constants.swift
@@ -10,4 +10,4 @@ import Foundation
 
 #warning("Provide either auth-url or auth-token to start the meeting")
 public let AUTH_URL = "https://dev-ios-api.zujonow.com"
-public let AUTH_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlrZXkiOiIxYTgyNjA2YS02MjcyLTQzZWItODZiYy0xYjE1OWM1ZDE2MWIiLCJwZXJtaXNzaW9ucyI6WyJhbGxvd19qb2luIl0sImlhdCI6MTY0MjgzNDc1NywiZXhwIjoxNjc0MzcwNzU3fQ.KaGDW7ynydgRhx3bnQbz70enRrVunO2WPxvyMILWkrQ"
+public let AUTH_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlrZXkiOiIxYTgyNjA2YS02MjcyLTQzZWItODZiYy0xYjE1OWM1ZDE2MWIiLCJwZXJtaXNzaW9ucyI6WyJhbGxvd19qb2luIiwiYWxsb3dfbW9kIl0sImlhdCI6MTY0MzM2MDUyMH0.wKmM_if3U3ef-FNWqROo0Mj_tqIvx1hIxT285ho8xAw"
