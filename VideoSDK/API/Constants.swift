e2b7b5cd435f8c62fb6e3b86ec70e93a78838f41
Rushi Sangani <rushi@rosterbuster.com>
2022-01-28 15:02:55 +0530

    code refactor

--- VideoSDK/API/Constants.swift
+++ VideoSDK/API/Constants.swift
@@ -9,5 +9,5 @@
 import Foundation
 
 #warning("Provide either auth-url or auth-token to start the meeting")
-public let AUTH_URL = "https://dev-ios-api.zujonow.com"
-public let AUTH_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlrZXkiOiIxYTgyNjA2YS02MjcyLTQzZWItODZiYy0xYjE1OWM1ZDE2MWIiLCJwZXJtaXNzaW9ucyI6WyJhbGxvd19qb2luIiwiYWxsb3dfbW9kIl0sImlhdCI6MTY0MzM2MDUyMH0.wKmM_if3U3ef-FNWqROo0Mj_tqIvx1hIxT285ho8xAw"
+public let AUTH_URL = ""
+public let AUTH_TOKEN = ""
