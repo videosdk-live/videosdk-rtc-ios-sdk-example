a52641b4b8f08a5975d23987709ef27b6063506d
Parth Asodariya <parth.asondariya@dhiwise.com>
2023-07-21 22:53:15 +0530

    Done: removed value of constants

--- VideoSDK/API/Constants.swift
+++ VideoSDK/API/Constants.swift
@@ -10,4 +10,4 @@ import Foundation
 
 #warning("Provide either auth-url or auth-token to start the meeting")
 public let AUTH_URL = ""
-public let AUTH_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlrZXkiOiI0N2M3ZTJlYy01NzY5LTQ3OWQtYjdjNS0zYjU5MDcxYzhhMDkiLCJwZXJtaXNzaW9ucyI6WyJhbGxvd19qb2luIl0sImlhdCI6MTY3NjQ2NTgwMCwiZXhwIjoxODM0MjUzODAwfQ.b9JUWkfImLFpS85WABCmJALg6kDpFJe3JHfCTOVIZk4"
+public let AUTH_TOKEN = ""
